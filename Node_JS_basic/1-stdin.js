0d4fe26c6df5342da21637ee2aef48f6d8e00723
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-10-14 22:31:37 -0400

    fix?

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,11 @@
 console.log('Welcome to Holberton School, what is your name?');
-
 // await input
 process.stdin.resume();
-
-// Define a variable to store user input
-let name = '';
-
-// Once input is entered
-process.stdin.on('data', (data) => {
-  name += data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  
-  // Check if input is coming from the terminal
+// once input is entered
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  // check if input is coming from the terminal
   if (process.stdin.isTTY) {
     process.exit();
   } else {
